766864ef2f90bf8e412ed1b106eba5658bee1a7e
Tim McGilchrist <timmcgil@gmail.com>
2018-03-09 17:35:33 +1100

    fixup! Fix lexing of multi-line string  literals.

--- src/Huck/Data/Token.hs
+++ src/Huck/Data/Token.hs
@@ -83,7 +83,7 @@ renderFloat = fromBuilder . Lazy.realFloat
 renderString :: STRING -> Text
 renderString = \case
   RAW s -> s
-  BASIC s -> s
+  BASIC s -> "\"" <> s <> "\""
   BASIC_MULTI ss -> "\"\"\"" <> ss <> "\"\"\""
   LITERAL ss -> "'" <> ss <> "'"
   LITERAL_MULTI ss -> "'''" <> ss  <> "'''"
